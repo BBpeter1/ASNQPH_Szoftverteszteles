9deb07a1263e8db4a19b1175d0d17e6fbc1a223c
BBP <79256968+BBpeter1@users.noreply.github.com>
2022-11-14 18:13:38 +0100

    Update testing.py

--- testing.py
+++ testing.py
@@ -3,6 +3,12 @@
 def fib(n: int) -> int:
     return n if n < 2 else fib(n-1)+fib(n-2)
 
+def plus(n: int, b: int) -> int:
+    return n+b
+
 
 def test_fibonacci():
     assert fib(10) == 55
+
+def test_plus(
+    assert plus(5,5) == 10
